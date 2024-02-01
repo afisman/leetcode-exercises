5a276d36ae295bcf582450fbfd823df15f6c48ca
afisman <afisman@gmail.com>
2024-02-01 15:12:13 +0100

    merged sorted array problem

--- Array.js
+++ Array.js
@@ -143,3 +143,18 @@ var flat = function (arr, n) {
 
 };
 
+//Merge sorted arrays #88
+
+var merge = function (nums1, m, nums2, n) {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    while (j >= 0) {
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k--] = nums1[i--]
+        } else {
+            nums1[k--] = nums2[j--]
+        }
+    }
+};
