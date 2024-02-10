5499b7ff750788c1fa04683e254152f3255e6ca8
afisman <afisman@gmail.com>
2024-02-10 21:55:38 +0100

    new medium exercises

--- String.js
+++ String.js
@@ -217,3 +217,27 @@ var decodeMessage = function (key, message) {
 
     return newMessage
 };
+
+//Valid palindrome II #680
+
+var validPalindrome = function(s, corrections = 1) {
+     let lo = 0;
+  let hi = s.length - 1;
+  
+  while (lo < hi) {
+    if (s[lo] === s[hi]) {
+      lo++;
+      hi--;
+      continue;
+    }
+    
+    if (corrections === 0) {
+      return false;
+    }
+    
+    return validPalindrome(s.slice(lo, hi), 0) 
+      || validPalindrome(s.slice(lo + 1, hi + 1), 0);
+  }
+  
+  return true;
+};
