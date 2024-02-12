dc760f9ea1c9a66273e619b516f01dbacb49b864
afisman <afisman@gmail.com>
2024-02-12 16:05:30 +0100

    new exercises

--- String.js
+++ String.js
@@ -315,3 +315,21 @@ var isSubsequence = function (s, t) {
 
     return true;
 };
+
+//longest common subsequence #1143
+
+var longestCommonSubsequence = function (text1, text2) {
+    const [len1, len2] = [text1.length, text2.length];
+    const dp = Array.from({ length: len1 + 1 }, () => new Array(len2 + 1).fill(0))
+
+    for (let i = 1; i < len1 + 1; i++) {
+        for (let j = 1; j < len2 + 1; j++) {
+            if (text1[i - 1] === text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+            }
+        }
+    }
+    return dp[len1][len2]
+};
