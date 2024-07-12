b63a82ef9771333d4ef30681c7099c6c29093a9b
afisman <afisman@gmail.com>
2024-07-12 14:19:55 +0200

    new exercise

--- Array.js
+++ Array.js
@@ -760,3 +760,19 @@ var validMountainArray = function (arr) {
 
     return asc === false
 };
+
+//3162 Find the number of good pairs
+
+var numberOfPairs = function (nums1, nums2, k) {
+    let goodPairs = 0;
+
+    for (let i = 0; i < nums1.length; i++) {
+        for (let j = 0; j < nums2.length; j++) {
+            if (nums1[i] % (nums2[j] * k) === 0) {
+                goodPairs++;
+            }
+        }
+    }
+    return goodPairs;
+
+};
