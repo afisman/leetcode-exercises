5b6bbdb1114b1134786f0b51897d747ba9a7b8ce
afisman <afisman@gmail.com>
2024-07-17 10:41:06 +0200

    new exercise

--- Math.js
+++ Math.js
@@ -152,20 +152,34 @@ var passThePillow = function (n, time) {
 
 };
 
-//Ugly number
+//263 Ugly number
 
-var isUgly = function(n) {
-    while(n>1) {
-        if(n%2==0){
-            n=n/2;
-        } else if(n%3==0) {
-            n=n/3;
-        } else if(n%5==0) {
-            n=n/5;
+var isUgly = function (n) {
+    while (n > 1) {
+        if (n % 2 == 0) {
+            n = n / 2;
+        } else if (n % 3 == 0) {
+            n = n / 3;
+        } else if (n % 5 == 0) {
+            n = n / 5;
         } else {
             break;
         }
     }
 
-    return n==1;
+    return n == 1;
+};
+
+//326 Power of three
+
+var isPowerOfThree = function (n) {
+    while (n > 1) {
+        if (n % 3 == 0) {
+            n = n / 3
+        } else {
+            break;
+        }
+    }
+
+    return n == 1
 };
