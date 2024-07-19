5793cb3a6ff4b03ab52e9111e85aae62c5af76d0
afisman <afisman@gmail.com>
2024-07-19 09:50:58 +0200

    new exercise

--- DP.js
+++ DP.js
@@ -46,3 +46,14 @@ var maxProfit = function (prices) {
     }
     return maxProfit;
 };
+
+//746 Min cost of climbing stairs
+
+var minCostClimbingStairs = function (cost) {
+    cost.push(0);
+    for (let i = cost.length - 4; i >= 0; i--) {
+        cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+    }
+
+    return Math.min(cost[0], cost[1])
+};
