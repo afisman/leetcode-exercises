cc946467cbd6de46d426b210b561ed5aac2b6135
afisman <afisman@gmail.com>
2024-07-30 18:40:00 +0200

    new exercise

--- Array.js
+++ Array.js
@@ -776,3 +776,20 @@ var numberOfPairs = function (nums1, nums2, k) {
     return goodPairs;
 
 };
+
+//3184 Count pairs that form a complete day I
+
+var countCompleteDayPairs = function (hours) {
+    let count = 0;
+
+    for (let i = 0; i < hours.length - 1; i++) {
+        for (let j = i + 1; j < hours.length; j++) {
+            if ((hours[i] + hours[j]) % 24 == 0) {
+                count++
+            }
+        }
+    }
+
+    return count
+
+};
