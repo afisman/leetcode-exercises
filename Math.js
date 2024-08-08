fc8de4c7c377f750c6e64d66366d8fc63b145c14
afisman <afisman@gmail.com>
2024-08-08 23:16:39 +0200

    new exercise

--- Math.js
+++ Math.js
@@ -202,3 +202,21 @@ var isPowerOfFour = function (n) {
 var distinctIntegers = function (n) {
     return n == 1 ? 1 : n - 1
 };
+
+//2894 difference of sums
+
+var differenceOfSums = function (n, m) {
+    let num1 = 0;
+    let num2 = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if (i % m == 0) {
+            num2 += i
+        } else {
+            num1 += i
+        }
+    }
+
+    return num1 - num2;
+
+};
