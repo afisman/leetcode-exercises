dc4ee6db89391260dd3ae989cba3982cf5f10f6b
afisman <afisman@gmail.com>
2024-08-16 20:50:30 +0200

    new exercise

--- Array.js
+++ Array.js
@@ -967,3 +967,16 @@ var minimumDifference = function (nums, k) {
 
     return minDiff;
 };
+
+//3190Minimum number of operations to make every element divisible by three
+
+var minimumOperations = function (nums) {
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 3 != 0) {
+            count++;
+        }
+    }
+    return count;
+};
