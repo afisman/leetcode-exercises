4d61f9fa1eb3c9afbd6271ac73d6187b05cd5868
afisman <afisman@gmail.com>
2024-08-20 13:10:55 +0200

    new exercise

--- Array.js
+++ Array.js
@@ -980,3 +980,16 @@ var minimumOperations = function (nums) {
     }
     return count;
 };
+
+//2798 Number of employees who met the target
+
+var numberOfEmployeesWhoMetTarget = function (hours, target) {
+    let count = 0;
+
+    for (let i = 0; i < hours.length; i++) {
+        if (hours[i] >= target) {
+            count++;
+        }
+    }
+    return count;
+};
