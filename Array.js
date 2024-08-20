27eda32794ddcb6f61329b21a1e5e0a18ca7c9a1
afisman <afisman@gmail.com>
2024-08-20 17:41:30 +0200

    new exercise

--- Array.js
+++ Array.js
@@ -1020,3 +1020,22 @@ var findPeaks = function (mountain) {
     return arr;
 
 };
+
+//2073 Time required to buy
+
+var timeRequiredToBuy = function (tickets, k) {
+    let time = 0;
+
+    for (let i = 0; i < tickets.length; i++) {
+        if (i < k) {
+            time += Math.min(tickets[i], tickets[k]);
+        } else if (i > k) {
+            time += Math.min(tickets[i], tickets[k] - 1);
+        } else {
+            time += tickets[k];
+        }
+    }
+
+    return time;
+
+};
