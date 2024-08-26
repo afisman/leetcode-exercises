4d441a3829bcbfe4a22ebb33f019763dfe10ceda
afisman <afisman@gmail.com>
2024-08-26 17:16:53 +0200

    new exercise

--- Array.js
+++ Array.js
@@ -1181,3 +1181,20 @@ var minimumDeletions = function (nums) {
 
     return Math.min(bothRemove, leftRemove, rightRemove)
 };
+
+//2824 Count pairs whose sum is less than target
+
+var countPairs = function (nums, target) {
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+
+            if (nums[i] + nums[j] < target) {
+                count++;
+            }
+        }
+    }
+
+    return count
+};
